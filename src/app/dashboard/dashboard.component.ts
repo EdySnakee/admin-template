6a7f3f45cce7b8e71ab0afdc74219b6a54591625
EdySnakee <contactoedysnake@gmail.com>
2024-05-01 13:05:21 -0600

    build(admin): :sparkles: init template

---
+++ src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,17 @@
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-dashboard',
+  standalone: true,
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    RouterLink
+  ],
+  templateUrl: './dashboard.component.html',
+  styleUrl: './dashboard.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class DashboardComponent { }
